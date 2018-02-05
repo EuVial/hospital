05f4f2c3afc01a1c56be96fde0f18c2276f0a181
EuVial <eugen.vial@gmail.com>
2018-02-05 17:30:40 +0300

    Bug fixed

--- src/main/java/controller/Action.java
+++ src/main/java/controller/Action.java
@@ -7,16 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-abstract class Action {
+public abstract class Action {
     private ServiceFactory serviceFactory;
 
     public final ServiceFactory getServiceFactory() {
         return serviceFactory;
     }
 
-    public final void setServiceFactory(final ServiceFactory serviceFactory) {
+    final void setServiceFactory(final ServiceFactory serviceFactory) {
         this.serviceFactory = serviceFactory;
     }
 
-    abstract Forward execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException;
+    public abstract Forward execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException;
 }
